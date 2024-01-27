241a00447963d9942a1f69a6458a2892f416d5bb
Musta1song <147011583+Musta1song@users.noreply.github.com>
2024-01-27 05:33:03 -0500

    Update landeshauptstadt.component.ts

--- src/app/Components/landeshauptstadt/landeshauptstadt.component.ts
+++ src/app/Components/landeshauptstadt/landeshauptstadt.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ArraysService } from 'src/app/Services/arrays/arrays.service';
 
 @Component({
